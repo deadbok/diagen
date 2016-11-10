99d5f93e7021936ae5fc34c1aac8bf356d567f37
moz <moz@local>
2016-11-10 22:04:28 +0100

    converted functions to a class

--- outputDot.py
+++ outputDot.py
@@ -6,27 +6,28 @@ https://github.com/moozer/nwdesign/blob/master/nwdesign/graphOut/l2Graph.py
 import functools
 import graphviz as gv
 
-def newGraph():
-    return functools.partial(gv.Graph, format='png', engine="neato")()
+class L2Diagram:
+    def __init__( self ):
+        self._graph = functools.partial(gv.Graph, format='png', engine="neato")()
 
-def addNodes( graph, nodes ):
-    # aplphabetically sorted node list
-    nodes = sorted( nodes )
+    def addNodes( self, nodes ):
+        # aplphabetically sorted node list
+        nodes = sorted( nodes )
 
-    # first add all nodes
-    for node in nodes:
-        graph.node( node )
+        # first add all nodes
+        for node in nodes:
+            self._graph.node( node )
 
-def addConnections( graph, connections ):
-    for conn in connections:
-        graph.edge( conn[0], conn[1] )
+    def addConnections( self, connections ):
+        for conn in connections:
+            self._graph.edge( conn[0], conn[1] )
 
-def output( graph, graphname ):
-    graph.render( "%s.%s"%(graphname, 'dot') )
-    graph.save( "%s.dot"%graphname )
+    def generateOutput( self, graphname ):
+        self._graph.render( "%s.%s"%(graphname, 'dot') )
+        self._graph.save( "%s.dot"%graphname )
 
-if __name__ == "__main__":    
-    graph = newGraph()
-    addNodes( graph, ["nodeA", "nodeB"])
-    addConnections( graph, [("nodeA", "nodeB")])
-    output( graph, "test")
+if __name__ == "__main__":
+    graph = L2Diagram()
+    graph.addNodes( ["nodeA", "nodeB"])
+    graph.addConnections( [("nodeA", "nodeB")])
+    graph.generateOutput( "test")
