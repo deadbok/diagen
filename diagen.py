9d3c28148be7c5bb2d05c2f2d789425ba4908ff5
moz <moz@local>
2016-11-13 13:27:34 +0100

    retactor for explainability

--- diagen.py
+++ diagen.py
@@ -1,16 +1,14 @@
 from readYaml import readYaml
 from outputDot import L2Diagram
 
-if __name__ == "__main__":
-    topology = readYaml( "test/inputA.yml" )
 
-    graph = L2Diagram()
-
+def addNodes( graph, topology ):
     print "Adding devices"
     nodes = topology.keys()
     print "- ", nodes
     graph.addNodes( nodes )
 
+def addConnections( graph, topology ):
     print "Adding connections"
     for device in topology:
         print "- ", device, " -> ", topology[device]["interfaces"]
@@ -19,5 +17,17 @@ if __name__ == "__main__":
             #print localInterface
             conn = (device+"."+localInterface, topology[device]["interfaces"][localInterface])
             graph.addConnections( [conn] )
-            
+
+if __name__ == "__main__":
+    # init part
+    graph = L2Diagram()
+
+    # handle inpu
+    topology = readYaml( "test/inputA.yml" )
+
+    # "business logic"
+    addNodes( graph, topology )
+    addConnections( graph, topology )
+
+    # output step
     graph.generateOutput("test")
